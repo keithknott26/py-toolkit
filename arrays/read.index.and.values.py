ea22acc17d27ca4ad627bc955680cc03967b90cf
Keith Knott <keith.knott@gmail.com>
2022-01-18 11:11:27 -0500

    Minor update to add index and values and century from year

---
+++ arrays/read.index.and.values.py
@@ -0,0 +1,2 @@
+def indexAndValues(listA)
+  return ([list((i, listA[i])) for i in range(len(listA))])
---
+++ time/read.century.from.year.py
@@ -0,0 +1,4 @@
+def readCenturyFromYear(year):
+  return (year-1)//100 +1
+
+#print(readCenturyFromYear(2017))
